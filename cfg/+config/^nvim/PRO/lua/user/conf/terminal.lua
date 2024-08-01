3a51bee34f36a9d322d6304200c6363b0d83079a
andyl <andy@r210.com>
2024-07-31 19:46:54 -0700

    Rewrite LazyGit command

--- cfg/+config/^nvim/PRO/lua/user/conf/terminal.lua
+++ cfg/+config/^nvim/PRO/lua/user/conf/terminal.lua
@@ -29,30 +29,27 @@ end
 
 vim.api.nvim_create_user_command('LazyGit', function()
   vim.cmd('tabnew')                 -- Open a new tab
+
+  vim.bo.bufhidden = 'wipe'
+  vim.wo.winfixwidth = true
+  vim.wo.winfixheight = true
+
   vim.cmd('wincmd o')               -- Set the buffer to full width
   vim.fn.termopen('lazygit', {      -- Open terminal and run lazygit
     on_exit = function(_, _, _)
       local events = require("neo-tree.events")
-      events.fire_event(events.GIT_EVENT)
-      vim.cmd('tabclose')           -- Close the tab when lazygit exits
-      vim.cmd("echo 'bye'")
+      events.fire_event(events.GIT_EVENT)                 -- Reset git/neo-tree
+      vim.cmd('tabclose')                                 -- Close the terminal tab
     end
   })
 
   vim.api.nvim_win_set_width(0, vim.o.columns)            -- Take the full width
+  vim.api.nvim_win_set_height(0, vim.o.lines - 1)
+
   vim.api.nvim_win_set_option(0, 'number', false)         -- Remove line numbers
   vim.api.nvim_win_set_option(0, 'relativenumber', false) -- Remove line numbers
+  vim.api.nvim_win_set_option(0, 'signcolumn', 'no')      -- TBD
+
   vim.cmd('startinsert')                                  -- Switch to insert mode
 end, {})
-
--- vim.api.nvim_create_autocmd({ "BufLeave" }, {
--- 	group    = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
--- 	pattern  = { "*lazygit*" },
--- 	callback = function()
---     vim.cmd("echo 'LEAVE'")
---     -- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1253#discussioncomment-9971975
---     local events = require("neo-tree.events")
---     events.fire_event(events.GIT_EVENT)
--- 	end,
--- })
 
