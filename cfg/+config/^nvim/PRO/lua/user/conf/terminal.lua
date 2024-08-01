7ae93b3a2c3ed15e0ad0f2a25629e0cfeeac293d
andyl <andy@r210.com>
2024-07-31 19:44:41 -0700

    Suppress message

--- cfg/+config/^nvim/PRO/lua/user/conf/terminal.lua
+++ cfg/+config/^nvim/PRO/lua/user/conf/terminal.lua
@@ -27,25 +27,32 @@ end
 
 -- Lazygit Command
 
-function LazyGit()
-  -- https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#custom-terminals
-  local opts = {
-    cmd           = "lazygit",
-    dir           = "git_dir",
-    hidden        = true,
-    direction     = "tab",
-    close_on_exit = true,
-  }
-  local term = require('toggleterm.terminal').Terminal:new(opts)
-  term:toggle()
-end
+vim.api.nvim_create_user_command('LazyGit', function()
+  vim.cmd('tabnew')                 -- Open a new tab
+  vim.cmd('wincmd o')               -- Set the buffer to full width
+  vim.fn.termopen('lazygit', {      -- Open terminal and run lazygit
+    on_exit = function(_, _, _)
+      local events = require("neo-tree.events")
+      events.fire_event(events.GIT_EVENT)
+      vim.cmd('tabclose')           -- Close the tab when lazygit exits
+      vim.cmd("echo 'bye'")
+    end
+  })
 
-vim.api.nvim_create_autocmd({ "BufLeave" }, {
-	group    = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
-	pattern  = { "*lazygit*" },
-	callback = function()
-    -- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1253#discussioncomment-9971975
-    local events = require("neo-tree.events")
-    events.fire_event(events.GIT_EVENT)
-	end,
-})
+  vim.api.nvim_win_set_width(0, vim.o.columns)            -- Take the full width
+  vim.api.nvim_win_set_option(0, 'number', false)         -- Remove line numbers
+  vim.api.nvim_win_set_option(0, 'relativenumber', false) -- Remove line numbers
+  vim.cmd('startinsert')                                  -- Switch to insert mode
+end, {})
+
+-- vim.api.nvim_create_autocmd({ "BufLeave" }, {
+-- 	group    = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
+-- 	pattern  = { "*lazygit*" },
+-- 	callback = function()
+--     vim.cmd("echo 'LEAVE'")
+--     -- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1253#discussioncomment-9971975
+--     local events = require("neo-tree.events")
+--     events.fire_event(events.GIT_EVENT)
+-- 	end,
+-- })
+
--- cfg/+config/^nvim/PRO/lua/user/lazy/msg_noice.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy/msg_noice.lua
@@ -4,7 +4,6 @@
 --
 -- https://github.com/folke/noice.nvim
 --
---
 
 local function skip_notifications_with(term)
   return { filter = { event = "notify", find = term }, opts = { skip = true } }
@@ -28,6 +27,7 @@ local opts = {
         skip_notifications_with("toggleterm"),
         skip_messages_with("written"),
         skip_messages_with("terminal.lua"),
+        skip_messages_with("only one window"),
       }
     })
   end
