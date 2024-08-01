d6f2e653b73671ebb00d813b1cf726fce959656f
andyl <andy@r210.com>
2024-07-31 19:56:48 -0700

    Update LazyGit function

--- cfg/+config/^nvim/PRO/lua/user/conf/terminal.lua
+++ cfg/+config/^nvim/PRO/lua/user/conf/terminal.lua
@@ -28,28 +28,25 @@ end
 -- Lazygit Command
 
 vim.api.nvim_create_user_command('LazyGit', function()
-  vim.cmd('tabnew')                 -- Open a new tab
+  vim.cmd('tabnew')              -- Open a new tab
+  vim.bo.bufhidden = 'wipe'      -- Clear prior state
+  vim.wo.winfixwidth = true      -- Set fixwidth
+  vim.wo.winfixheight = true     -- Set fixheight
+  vim.cmd('wincmd o')            -- Set buffer to full width
 
-  vim.bo.bufhidden = 'wipe'
-  vim.wo.winfixwidth = true
-  vim.wo.winfixheight = true
-
-  vim.cmd('wincmd o')               -- Set the buffer to full width
-  vim.fn.termopen('lazygit', {      -- Open terminal and run lazygit
-    on_exit = function(_, _, _)
-      local events = require("neo-tree.events")
-      events.fire_event(events.GIT_EVENT)                 -- Reset git/neo-tree
-      vim.cmd('tabclose')                                 -- Close the terminal tab
+  vim.fn.termopen('lazygit', {                  -- Open terminal and run lazygit
+    on_exit = function(_, _, _)                 -- Define exit handler
+      local events = require("neo-tree.events") -- Reset git/neo-tree
+      events.fire_event(events.GIT_EVENT)       -- Reset git/neo-tree
+      vim.cmd('tabclose')                       -- Close the terminal tab
     end
   })
 
-  vim.api.nvim_win_set_width(0, vim.o.columns)            -- Take the full width
-  vim.api.nvim_win_set_height(0, vim.o.lines - 1)
-
+  vim.api.nvim_win_set_width(0, vim.o.columns)            -- Set full-width
+  vim.api.nvim_win_set_height(0, vim.o.lines - 1)         -- Set full-height
   vim.api.nvim_win_set_option(0, 'number', false)         -- Remove line numbers
   vim.api.nvim_win_set_option(0, 'relativenumber', false) -- Remove line numbers
-  vim.api.nvim_win_set_option(0, 'signcolumn', 'no')      -- TBD
-
+  vim.api.nvim_win_set_option(0, 'signcolumn', 'no')      -- Remove signcolumn
   vim.cmd('startinsert')                                  -- Switch to insert mode
 end, {})
 
