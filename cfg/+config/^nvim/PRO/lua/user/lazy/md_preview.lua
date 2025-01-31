aa9689e954b22c83a592c7c1bb088c43b26b63ee
andyl <andy@r210.com>
2025-01-30 18:53:14 -0800

    250130-1853

--- cfg/+config/^nvim/PRO/lua/user/lazy/md_preview.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy/md_preview.lua
@@ -30,10 +30,10 @@
 -- USE THIS
 
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd   = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft    = { "markdown" },
-  init  = function() vim.g.mkdp_filetypes = { "markdown" } end,
-  build = "cd app && npm install",
+  -- "iamcco/markdown-preview.nvim",
+  -- cmd   = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  -- ft    = { "markdown" },
+  -- init  = function() vim.g.mkdp_filetypes = { "markdown" } end,
+  -- build = "cd app && npm install",
 }
 
