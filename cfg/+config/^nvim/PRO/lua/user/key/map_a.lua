649948439f19adaae13ca438c2cf28a9c8311516
andyl <andy@r210.com>
2025-03-26 18:41:59 -0700

    Add ad for dash alignment

--- cfg/+config/^nvim/PRO/lua/user/key/map_a.lua
+++ cfg/+config/^nvim/PRO/lua/user/key/map_a.lua
@@ -2,6 +2,8 @@
 
 -- at = align table
 -- usage: vipat
+-- ad = align dash
+-- usage: vipad
 -- ac = align character
 -- usage: vipac
 -- af = align format
@@ -9,7 +11,12 @@
 
 vim.cmd ":nmap at :EasyAlign *\\|<cr>"
 vim.cmd ":vmap at :EasyAlign *\\|<cr>"
+vim.cmd ":nmap ad :EasyAlign */\\-/<cr>"
+vim.cmd ":vmap ad :EasyAlign */\\-/<cr>"
 vim.cmd ":nmap ac :EasyAlign "
 vim.cmd ":vmap ac :EasyAlign "
 vim.cmd ":nmap af :MixFormat"
 
+-- dash alignment isn't working properly
+-- don't align on first dash
+-- align -> like a single dash
