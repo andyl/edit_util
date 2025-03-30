83702ba38c4fc9b3522d65c5d4d77d7318ad1aea
andyl <andy@r210.com>
2025-03-29 22:23:46 -0700

    250329-2223

--- cfg/+config/^nvim/PRO/lua/user/lazy_list.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy_list.lua
@@ -42,7 +42,7 @@ local opts = {
 	"axkirillov/easypick.nvim",           -- simple custom telescope pickers
   -- AI Agents
   require("user.lazy.aider"),           -- aider: ML / AI interface
-	-- require("user.lazy.avante"),          -- avante: Cursor-like ML / AI interface
+	require("user.lazy.avante"),          -- avante: Cursor-like ML / AI interface
 	-- NGINX
 	"chr4/nginx.vim",                     -- nginx config files
 	-- DEBUGGING
