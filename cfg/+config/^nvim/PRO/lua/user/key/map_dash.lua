b714a25e1e17208a0c2cdbc3d17dbf42bfdda944
andyl <andy@r210.com>
2025-03-31 16:49:20 -0700

    Remove avante

--- cfg/+config/^nvim/PRO/lua/user/key/map_dash.lua
+++ cfg/+config/^nvim/PRO/lua/user/key/map_dash.lua
@@ -1,29 +1,29 @@
 -- key/map_dash
 
-local opts1 = {
-  {"-a", group = "Avante"                               },
-  {"-aa", ":AvanteAsk<cr>",     desc = "Avante Ask"     },
-  {"-ae", ":AvanteEdit<cr>",    desc = "Avante Edit"    },
-  {"-ar", ":AvanteRefresh<cr>", desc = "Avante Refresh" },
-  {"-at", ":AvanteToggle<cr>",  desc = "Avante Toggle"  },
-
-  {"-r", "<cmd>lua require('spectre').toggle()<cr>", desc = "Replace across Projects" },
-}
-
-WhichKey.add(opts1)
+-- local opts1 = {
+--   {"-a", group = "Avante"                               },
+--   {"-aa", ":AvanteAsk<cr>",     desc = "Avante Ask"     },
+--   {"-ae", ":AvanteEdit<cr>",    desc = "Avante Edit"    },
+--   {"-ar", ":AvanteRefresh<cr>", desc = "Avante Refresh" },
+--   {"-at", ":AvanteToggle<cr>",  desc = "Avante Toggle"  },
+--
+--   {"-r", "<cmd>lua require('spectre').toggle()<cr>", desc = "Replace across Projects" },
+-- }
+--
+-- WhichKey.add(opts1)
 
 -------------------------------------------------------------------------
 
-local opts2 = {
-  {
-    mode = { "v" },
-
-    {"-a", group = "Avante"                         },
-    {"-ae", ":AvanteEdit<cr>", desc = "Avante Edit" },
-  }
-}
-
-WhichKey.add(opts2)
+-- local opts2 = {
+--   {
+--     mode = { "v" },
+--
+--     {"-a", group = "Avante"                         },
+--     {"-ae", ":AvanteEdit<cr>", desc = "Avante Edit" },
+--   }
+-- }
+--
+-- WhichKey.add(opts2)
 
 -------------------------------------------------------------------------
 
