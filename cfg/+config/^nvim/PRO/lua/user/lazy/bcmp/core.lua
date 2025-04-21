7846bcf075c26775af14018834b9e7aecbe72cfb
andyl <andy@r210.com>
2025-04-20 18:38:51 -0700

    Add emoji completions

--- cfg/+config/^nvim/PRO/lua/user/lazy/bcmp/core.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy/bcmp/core.lua
@@ -2,7 +2,10 @@ local opts = {
   'saghen/blink.cmp',
 
   -- https://github.com/rafamadriz/friendly-snippets
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "moyiz/blink-emoji.nvim",
+  },
 
   -- release tag to download pre-built binaries
   version = '1.*',
@@ -20,24 +23,18 @@ local opts = {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    -- keymap = { preset = 'default' },
     keymap = { preset = 'enter' },
 
     signature = { enabled = true },
 
     appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
     },
 
-    -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = true } },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'snippets', 'lsp', 'path', 'buffer' },
+      default = { 'snippets', 'lsp', 'path', 'emoji', 'buffer' },
       providers = {
         lsp = {
           name = "LSP",
@@ -54,6 +51,12 @@ local opts = {
           module = "blink.cmp.sources.snippets",
           score_offset = 50,
         },
+        emoji = {
+          name = "emoji",
+          module = "blink-emoji",
+          score_offset = 25,
+          opts = { insert = true },
+        },
         buffer = {
           name = "Buffer",
           module = "blink.cmp.sources.buffer",
@@ -69,11 +72,6 @@ local opts = {
       }
     },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
     fuzzy = { implementation = "prefer_rust_with_warning" }
   },
   opts_extend = { "sources.default" }
