11977b469186d3b6bf92592af4e4498b794f6710
andyl <andy@r210.com>
2025-06-28 09:40:12 -0700

    Reformat

--- cfg/+config/^nvim/PRO/lua/user/lazy/auto_session.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy/auto_session.lua
@@ -2,23 +2,29 @@
 --
 -- AI Pair-Programming Tool
 --
+-- # AIDER
+--
 -- https://aider.chat
+--
+-- Use `install_aider` to install or update...
+-- Run aider --help to see all the options you can pass to the CLI.
+--
+-- # AIDER.NVIM
+--
 -- https://github.com/joshuavial/aider.nvim
-
+--
 -- Commands:
 -- :AiderOpen
 -- :AiderOpen -3 hsplit
 -- :AiderOpen "AIDER_NO_AUTO_COMMITS=1 aider -3" editor
 -- :AiderAddModifiedFiles
 --
--- Run aider --help to see all the options you can pass to the CLI.
---
 -- The plugin provides the following default keybindings:
 -- <leader>Ao: Open a terminal window with the Aider defaults (gpt-4).
 -- <leader>Am: Add all git-modified files to the Aider chat.
 -- <leader><space>: Open Aider (Deprecated - will be removed in a future version)
 -- These keybindings are set up using which-key, providing a descriptive popup menu when you press <leader>A.
-
+--
 -- Tips for Working with Buffers in Vim
 -- :ls or :buffers to see all open buffers.
 -- :b <number> or :buffer <number> to switch to a specific buffer. Replace <number> with the buffer number.
