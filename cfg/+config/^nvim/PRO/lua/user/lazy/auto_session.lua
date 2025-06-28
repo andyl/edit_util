2821ef072899c1357a9698be6fa19130bdb9519d
andyl <andy@r210.com>
2025-06-28 10:57:34 -0700

    Rename files

--- cfg/+config/^nvim/PRO/lua/user/lazy/auto_session.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy/auto_session.lua
@@ -1,45 +1,18 @@
--- aider
+-- auto_session
 --
--- AI Pair-Programming Tool
+-- Automatic session management
 --
--- # AIDER
---
--- https://aider.chat
---
--- Use `install_aider` to install or update...
--- Run aider --help to see all the options you can pass to the CLI.
---
--- # AIDER.NVIM
---
--- https://github.com/joshuavial/aider.nvim
---
--- Commands:
--- :AiderOpen
--- :AiderOpen -3 hsplit
--- :AiderOpen "AIDER_NO_AUTO_COMMITS=1 aider -3" editor
--- :AiderAddModifiedFiles
---
--- The plugin provides the following default keybindings:
--- <leader>Ao: Open a terminal window with the Aider defaults (gpt-4).
--- <leader>Am: Add all git-modified files to the Aider chat.
--- <leader><space>: Open Aider (Deprecated - will be removed in a future version)
--- These keybindings are set up using which-key, providing a descriptive popup menu when you press <leader>A.
---
--- Tips for Working with Buffers in Vim
--- :ls or :buffers to see all open buffers.
--- :b <number> or :buffer <number> to switch to a specific buffer. Replace <number> with the buffer number.
--- :bd or :bdelete to close the current buffer.
--- :bd <number> or :bdelete <number> to close a specific buffer. Replace <number> with the buffer number.
--- :bufdo bd to close all buffers.
+-- https://github.com/rmagatti/auto-session
+-- https://youtu.be/c9y7bKk-R7U?t=365
 
 local lcl_opts = {
-  "joshuavial/aider.nvim",
+  "rmagatti/auto-session",
+  lazy = false,
+
   opts = {
-    auto_manage_context = true, -- automatically manage buffer context
-    default_bindings = true,    -- use default <leader>A keybindings
-    debug = false,              -- enable debug logging
-    ignore_buffers = {},        -- list of buffers to ignore
-  },
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    bypass_save_filetypes = { 'neo-tree', 'help' },
+  }
 }
 
 return lcl_opts
