0ffefcf32cc270ab180bd955693f28f503debfa9
andyl <andy@r210.com>
2025-07-21 05:52:32 -0700

    250721-0552

--- cfg/+config/^nvim/PRO/lua/user/plug/speeddating.lua
+++ cfg/+config/^nvim/PRO/lua/user/plug/speeddating.lua
@@ -2,6 +2,7 @@
 --------------------------------------------------------
 
 -- SpeedDatingFormat - increment/decrement dates with c-x/c-a
+-- https://github.com/tpope/vim-speeddating
 
 vim.cmd "1SpeedDatingFormat %Y %h %d %i"
 
@@ -13,7 +14,8 @@ local function zdir(direction)
 end
 
 -- "g_"    | move to last non-blank character
--- "<C-?>" | increment/decrement
+-- "<C-x>" | increment
+-- "<C-a>" | decrement
 
 local keymap = vim.api.nvim_set_keymap
 
@@ -23,4 +25,16 @@ keymap("i", "<M-u>", "<esc>g_<C-x>a", zdir("decrement")) -- move date up
 keymap("i", "<M-o>", "<esc>g_<C-a>a", zdir("increment")) -- move date out
 
 -- try it!
+--
 -- 2025 May 29 Thu
+--
+-- Fri, 31 Dec 1999 23:59:59 +0000
+-- Fri Dec 31 23:59:59 UTC 2001
+-- 2008-01-05T04:59:59Z
+-- 1865-04-14
+-- 11/Sep/01
+-- January 14th, 1983
+-- 11:55 AM
+-- 3rd
+-- XXXVIII
+
