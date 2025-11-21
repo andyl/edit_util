3dc5e717a3f993e043963addd1bd662748e66313
andyl <andy@r210.com>
2025-11-20 23:24:01 -0800

    251120-2323

--- cfg/+config/^nvim/PRO/lua/user/lazy_boot.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy_boot.lua
@@ -2,6 +2,7 @@
 --
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -12,5 +13,6 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
