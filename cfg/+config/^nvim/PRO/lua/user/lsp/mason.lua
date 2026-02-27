3c6e0128ca7d74c8e19f1cee306e05b97bd52fa6
andyl <andy@r210.com>
2026-02-27 09:52:05 -0800

    260227-0951

--- cfg/+config/^nvim/PRO/lua/user/lsp/mason.lua
+++ cfg/+config/^nvim/PRO/lua/user/lsp/mason.lua
@@ -36,22 +36,22 @@
 local server_list = {
   { name = "bashls",        cfg = false }, -- bash
   { name = "dockerls",      cfg = false }, -- dockerfile
-  -- { name = "elixirls",      cfg = false }, -- elixir
   { name = "expert",        cfg = false }, -- elixir | https://expert-lsp.org
   { name = "emmet_ls",      cfg = true  }, -- emmet
-  -- { name = "gopls",         cfg = false }, -- golang
+  { name = "gopls",         cfg = false }, -- golang
   { name = "html",          cfg = false }, -- html
   { name = "jsonls",        cfg = true  }, -- json
   { name = "lua_ls",        cfg = true  }, -- lua
   { name = "marksman",      cfg = false }, -- markdown
   { name = "pyright",       cfg = true  }, -- python
-  -- { name = "solargraph",    cfg = false }, -- ruby
+  { name = "solargraph",    cfg = false }, -- ruby
   { name = "sqlls",         cfg = false }, -- sql
   { name = "terraformls",   cfg = false }, -- hashicorp terraform/hcl
   { name = "tailwindcss",   cfg = true  }, -- tailwind
   { name = "ts_ls",         cfg = false }, -- javascript and typescript (does this work?)
-  -- { name = "yamlls",        cfg = true  }, -- yaml
+  { name = "yamlls",        cfg = true  }, -- yaml
   { name = "zls",           cfg = false }, -- zig
+  -- { name = "elixirls",      cfg = false }, -- elixir
 }
 
 local function all_servers()
