1a5104e3114d673420088a0715317fa9ccf48ef3
andyl <andy@r210.com>
2026-03-02 10:09:07 -0800

    Update lsp_config colors

--- cfg/+config/^nvim/PRO/lua/user/lazy/lsp_config.lua
+++ cfg/+config/^nvim/PRO/lua/user/lazy/lsp_config.lua
@@ -1,35 +1,51 @@
 -- lsp_config
 --
--- Quickstart configs for NVIM LSP
--- Color display for CSS & Tailwind classes
 
 return {
-
   {
+    -- Quickstart configs for NVIM LSP
     -- https://github.com/neovim/nvim-lspconfig
     -- : h lspconfig.txt
     'neovim/nvim-lspconfig'
   },
-
   {
+    -- Color display for CSS & Tailwind classes
     -- https://github.com/NvChad/nvim-colorizer.lua
     -- https://www.youtube.com/watch?v=_NiWhZeR-MY&t=30s
     'NvChad/nvim-colorizer.lua',
     config = function()
       require("colorizer").setup({
-        user_default_options = {
-          css         = true,
-          RGB         = true,
-          RRGGBB      = true,
-          tailwind    = true,
-          virtualtext = "■",
+        options = {
+          parsers = {
+            css         = true,
+            css_fn      = true,
+            RGB         = { enable = true },
+            RRGGBB      = { enable = true },
+            tailwind    = { enable = true },
+            virtualtext = "■",
+          },
+          filetypes = {
+            '*',
+            '!md',
+            '!markdown'
+          }
         },
-        filetypes = {
-          '*',
-          '!md',
-          '!markdown'
-        }
       })
+      -- require("colorizer").setup({
+      --   suppress_deprecation = true,
+      --   user_default_options = {
+      --     css         = true,
+      --     RGB         = true,
+      --     RRGGBB      = true,
+      --     tailwind    = true,
+      --     virtualtext = "■",
+      --   },
+      --   filetypes = {
+      --     '*',
+      --     '!md',
+      --     '!markdown'
+      --   }
+      -- })
     end
   },
 
